1bea4853c7d72c5c21250d8a3eee13d2d4626d51
Julio <j.diaz@redcapital.cl>
2024-05-01 01:15:28 -0400

    add: documentacion completa swagger

--- app/Http/Controllers/API/BenefitController.php
+++ app/Http/Controllers/API/BenefitController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
@@ -7,18 +6,45 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-
+use OpenApi\Annotations as OA;
+/**
+ * @OA\Info(
+ *     title="Endpoint get Beneficios",
+ *     version="1.0.0",
+ *     description="Obtener beneficios filtrados y ordenados a partir de informacion obtenida de otros endpoints"
+ * )
+ */
 class BenefitController extends Controller
 {
     /**
      * @OA\Get(
-     *     path="/api/data",
+     *     path="/api/get_data",
      *     summary="Obtener datos de beneficios",
      *     tags={"Beneficios"},
      *     @OA\Response(response=200, description="Datos de beneficios obtenidos correctamente"),
      *     @OA\Response(response=404, description="Error al procesar la solicitud")
      * )
-     */
+     * @OA\Schema(
+     *     schema="Beneficio",
+     *     @OA\Property(property="id_programa", type="integer"),
+     *     @OA\Property(property="monto", type="number", format="float"),
+     *     @OA\Property(property="fecha_recepcion", type="string", format="date"),
+     *     @OA\Property(property="fecha", type="string", format="date"),
+     *     @OA\Property(property="anio", type="integer"),
+     *     @OA\Property(property="view", type="boolean"),
+     *     @OA\Property(property="ficha", ref="#/components/schemas/Ficha"),
+     * ),
+     * @OA\Schema(
+     *     schema="Ficha",
+     *     @OA\Property(property="id", type="integer"),
+     *     @OA\Property(property="nombre", type="string"),
+     *     @OA\Property(property="id_programa", type="integer"),
+     *     @OA\Property(property="url", type="string"),
+     *     @OA\Property(property="categoria", type="string"),
+     *     @OA\Property(property="descripcion", type="string"),
+     * )
+    */
+
     public function getData(){
         try {
             $beneficios = collect($this->getDataFromURL("https://run.mocky.io/v3/399b4ce1-5f6e-4983-a9e8-e3fa39e1ea71")['data']);
